08e86251fd991fec78940b6cdabf9a65fec14e90
jaspmedeX <159879056+jaspmedeX@users.noreply.github.com>
2026-04-05 23:53:33 -0400

    Add files via upload
    Add strategy pattern

--- app/strategies/projectSortStrategy.ts
+++ app/strategies/projectSortStrategy.ts
@@ -1,1 +1,37 @@
+import { Project } from '../data/mockData';
 
+export interface ProjectSortStrategy {
+  sort(projects: Project[]): Project[];
+}
+
+export class SortByDateStrategy implements ProjectSortStrategy {
+  sort(projects: Project[]): Project[] {
+    return [...projects].sort(
+      (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+    );
+  }
+}
+
+export class SortByTitleStrategy implements ProjectSortStrategy {
+  sort(projects: Project[]): Project[] {
+    return [...projects].sort((a, b) => a.title.localeCompare(b.title));
+  }
+}
+
+export class SortByCategoryStrategy implements ProjectSortStrategy {
+  sort(projects: Project[]): Project[] {
+    return [...projects].sort((a, b) => a.category.localeCompare(b.category));
+  }
+}
+
+export class ProjectSorter {
+  constructor(private strategy: ProjectSortStrategy) {}
+
+  setStrategy(strategy: ProjectSortStrategy) {
+    this.strategy = strategy;
+  }
+
+  sort(projects: Project[]): Project[] {
+    return this.strategy.sort(projects);
+  }
+}
